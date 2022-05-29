f922a1578a375a040899ab36a151bd64d478168c
surya211-suri <71824708+surya211-suri@users.noreply.github.com>
2022-05-29 14:42:31 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,25 +1,18 @@
 const client = require('./connection.js')
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-let port = process.env.PORT || 3000
+let port_no = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(port, () => {
-    console.log("Sever is now listening at port 3000");
+app.listen(port_no, () => {
+    console.log("Sever is now listening at port "+port_no);
 })
 
 client.connect();
 
 
 app.get('/', (req, res) => {
-    client.query(`Select * from branches limit 100`, (err, result) => {
-        if (!err) {
-            res.send(result.rows);
-        }
-    });
-    client.end;
+    
+    res.send("sample");
 })
 
 app.get('/api/branch', (req, res) => {
@@ -27,7 +20,7 @@ app.get('/api/branch', (req, res) => {
     var limit = req.query.limit;
     var offset = req.query.offset;
     console.log(query, limit, offset);
-    client.query(`Select * from branches where branch ilike '${query}%' limit ${limit} offset ${offset}`, (err, result) => {
+    client.query(`Select * from bank_branches where branch ilike '${query}%' limit ${limit} offset ${offset}`, (err, result) => {
         if (!err) {
             res.send(result.rows);
         }
@@ -43,7 +36,7 @@ app.get('/api/search', (req, res) => {
     var limit = req.query.limit;
     var offset = req.query.offset;
     console.log(query, limit, offset);
-    client.query(`Select * from branches where branch ilike '${query}%' or ifsc ilike '${query}%' or address ilike '${query}%' or city ilike '${query}%' or district ilike '${query}%' or state ilike '${query}%' or bank_name ilike '${query}%' limit ${limit} offset ${offset}`, (err, result) => {
+    client.query(`Select * from bank_branches where branch ilike '${query}%' or ifsc ilike '${query}%' or address ilike '${query}%' or city ilike '${query}%' or district ilike '${query}%' or state ilike '${query}%' or bank_name ilike '${query}%' limit ${limit} offset ${offset}`, (err, result) => {
         if (!err) {
             res.send(result.rows);
 
